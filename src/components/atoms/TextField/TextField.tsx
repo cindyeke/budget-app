8e891c6f1d18dfc591ba3bd94188dcf4db33a7b0
Cindy Eke <cindyeke@Cindys-MacBook-Pro.local>
2024-04-26 14:07:11 +0100

    feat: initial setup; colors, prettier config

---
+++ src/components/atoms/TextField/TextField.tsx
@@ -0,0 +1,7 @@
+const TextField = () => {
+    return (
+        <>textfield</>
+    )
+}
+
+export default TextField
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,14 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    colors: {
+      grayblack: '#OBOC10',
+      gray: '#C5C6C7',
+      turquoise: '#66FCF1',
+      teal: '#45A29E',
+      white: '#FFFFFF',
+      black: '#000000',
+    }
   },
   plugins: [],
 };
